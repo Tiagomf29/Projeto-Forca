5959120f44354fed127c29aa75784d70ba5a193a
tiagomf29 <tiagomf29@hotmail.com>
2021-04-15 21:01:11 -0300

    Ajustes jogo

--- scripts.js
+++ scripts.js
@@ -1,22 +1,34 @@
 
 var lista = [];
+var erro = 1;
 
 window.onload = function(){
-    let palavra = "Tiago Martins Ferreira";
-
+    
+	let palavra = "TESTE";
     for(let i = 0; i < palavra.length; i++){
-
         lista[i] = palavra.charAt(i);
-        insereCelula(palavra.charAt(i));
-
+        insereCelula(palavra.charAt(i),i);       
     }
-
-    incluiLetra(0,"C");
-    incluiLetra(5,"C");
+    eventosBotoes();
 };
 
+function validaSeVenceuOJOgo(){
 
-function insereCelula(letra){
+    let ok = document.querySelectorAll("td");
+    let naoFaltaValor = true;
+    ok.forEach(vlr =>{
+        if(vlr.textContent == ""){
+            naoFaltaValor = false;
+        }
+    });
+	
+	if(naoFaltaValor){
+      mensagemTerminoJogo("Parabéns. Você venceu o jogo!!!")
+    }
+}
+
+
+function insereCelula(letra,posicao){
 
     var t = document.querySelector("tr");
     var linha = document.createElement("td");    
@@ -25,16 +37,15 @@ function insereCelula(letra){
         linha.appendChild(texto);
         linha.style.textAlign="center";
         linha.style.backgroundColor="yellow";
-    }
-    t.insertBefore(linha, t.firstChild);
+    }   
     
+    t.appendChild(linha, t.firstChild);
 }
 
 
 function incluiLetra(posicao, letra){
 
     let linhaTabela = document.querySelectorAll("td");
-
     linhaTabela.forEach((vlr,index) =>{
         if(index==posicao){
             let texto = document.createTextNode(letra);            
@@ -42,5 +53,98 @@ function incluiLetra(posicao, letra){
             linhaTabela[index].style.textAlign="center";     
         }
     });
+}
+
+function eventosBotoes(){
+
+    let botoes = document.querySelectorAll("input");
+    botoes.forEach(btn=>{
+        btn.addEventListener('click', e =>{          
+            verificaExistenciaLetra(btn.value);
+            btn.disabled="disabled";
+        });
+    });
+}
+
+
+function verificaExistenciaLetra(letra){
+
+    let achou = false;
+    for(let i = 0; i < lista.length; i++){
+        if(lista[i].toLowerCase() == letra.toLowerCase()){
+            incluiLetra(i,letra);
+            achou = true;
+            lista[i]=" ";
+        }
+    }
+
+    if(!achou){
+        exibeImagem(erro);
+    }
+
+    validaSeVenceuOJOgo();
+}
+
+function exibeImagem(membro){
+
+    switch (membro) {
+        case 1:
+            document.getElementById("corda").style.display = "flex"; 
+            erro+=1;          
+            break;
+        case 2:
+            document.getElementById("cabeca").style.display = "flex";             
+            erro+=1;
+            break;
+        case 3:
+            document.getElementById("corpo").style.display = "flex";             
+            erro+=1;
+            break;
+        case 4:
+            document.getElementById("mao-direita").style.display = "flex";             
+            erro+=1;
+            break;
+        case 5:
+            document.getElementById("mao-esquerda").style.display = "flex";             
+            erro+=1;
+            break;
+        case 6:
+            document.getElementById("pe-direito").style.display = "flex";             
+            erro+=1;
+            break;
+        case 7:
+            document.getElementById("pe-esquerdo").style.display = "flex";            
+            erro+=1;
+            mensagemTerminoJogo("Não foi desta vez! Você perdeu.");
+            desabilitaTodosBotoes();
+            break;                                                                        
+    }
 
 }
+
+
+function mensagemTerminoJogo(mensagem){
+
+    let msg = document.getElementById("mensagem");    
+    msg.innerHTML = mensagem;
+    msg.style.fontSize = "40px";
+    msg.style.color = "red";
+
+    let divisao = document.getElementById("mensagemFinal");    
+    divisao.style.fontSize = "40px";
+    divisao.style.position = "absolute";
+    divisao.style.top = "40%";
+    divisao.style.left = "30%";
+    divisao.style.backgroundColor = "yellow";
+    divisao.style.borderRadius = "30px";
+}
+
+function desabilitaTodosBotoes(){
+
+    let desabilita = document.querySelectorAll("input");
+    desabilita.forEach(dsb=>{
+        dsb.disabled = "disabled";
+    });
+}
+
+
